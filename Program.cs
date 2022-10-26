75154f1c255410970cd2f121f8fe2868ac072eae
AnaFreitasNeves <102673714+AnaFreitasNeves@users.noreply.github.com>
2022-10-26 11:47:20 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+// início do arquivo
+using Microsoft.EntityFrameworkCore;
+using MvcLabManager.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// após builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<LabManagerContext>(options => 
+    options.UseMySQL("server=localhost;database=estudante;user=estudante;password=estudante")
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
